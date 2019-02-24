d82c0d6277e684110b3f96cbff6e8bd49eb546a5
prabhu <pshankar@virtusa.com>
2019-02-24 14:00:26 -0500

    test

---
+++ quickstart/src/main/java/com/prabhu/maven/quickstart/App.java
@@ -0,0 +1,19 @@
+package com.prabhu.maven.quickstart;
+
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App 	
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+    	
+    	System.out.println("adhvaith");
+    	System.out.print("hiya");
+    	
+        System.out.println( gson.toJson("Hello World!") );
+    }
+}
